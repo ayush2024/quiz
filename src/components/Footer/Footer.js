9a9a8c3393b4adc9a814c02771b40ed10dfdbabb
Ayush Jha <ayushkumarjha666@gmil.com>
2023-08-25 14:22:42 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 1,
+        color: 'white',
+        background: 'grey'
+      }}
+    >
+      Made by{" "}
+      <a
+        href=""
+        style={{ cursor: "pointer" }}
+      >
+        Ayush Kumar Jha
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
